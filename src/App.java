9b2b29e82afd8eed9dc9ea0fe0037c435e2f5172
Gabriel Mikus <gabriel.mikus11@gmail.com>
2023-03-07 10:49:27 -0500

    Montezuma start

--- src/App.java
+++ src/App.java
@@ -1,6 +1,33 @@
 public class App {
+    //░
+    //█
     public static void main(String[] args) {
-        System.out.println("hello");
+        afficherIntro();
+
+
+    }
+    public static void afficherIntro(){
+        System.out.println("© Gabriel Mikus" + "\u001B[32m");
+        System.out.println("\n" +
+                " ███▄ ▄███▓ ▒█████   ███▄    █ ▄▄▄█████▓▓█████ ▒███████▒ █    ██  ███▄ ▄███▓ ▄▄▄      \n" +
+                "▓██▒▀█▀ ██▒▒██▒  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒▓█   ▀ ▒ ▒ ▒ ▄▀░ ██  ▓██▒▓██▒▀█▀ ██▒▒████▄    \n" +
+                "▓██    ▓██░▒██░  ██▒▓██  ▀█ ██▒▒ ▓██░ ▒░▒███   ░ ▒ ▄▀▒░ ▓██  ▒██░▓██    ▓██░▒██  ▀█▄  \n" +
+                "▒██    ▒██ ▒██   ██░▓██▒  ▐▌██▒░ ▓██▓ ░ ▒▓█  ▄   ▄▀▒   ░▓▓█  ░██░▒██    ▒██ ░██▄▄▄▄██ \n" +
+                "▒██▒   ░██▒░ ████▓▒░▒██░   ▓██░  ▒██▒ ░ ░▒████▒▒███████▒▒▒█████▓ ▒██▒   ░██▒ ▓█   ▓██▒\n" +
+                "░ ▒░   ░  ░░ ▒░▒░▒░ ░ ▒░   ▒ ▒   ▒ ░░   ░░ ▒░ ░░▒▒ ▓░▒░▒░▒▓▒ ▒ ▒ ░ ▒░   ░  ░ ▒▒   ▓▒█░\n" +
+                "░  ░      ░  ░ ▒ ▒░ ░ ░░   ░ ▒░    ░     ░ ░  ░░░▒ ▒ ░ ▒░░▒░ ░ ░ ░  ░      ░  ▒   ▒▒ ░\n" +
+                "░      ░   ░ ░ ░ ▒     ░   ░ ░   ░         ░   ░ ░ ░ ░ ░ ░░░ ░ ░ ░      ░     ░   ▒   \n" +
+                "       ░       ░ ░           ░             ░  ░  ░ ░       ░            ░         ░  ░\n" +
+                "                                               ░                                      \n");
+        System.out.println("En pleine jungle aztèque, vous cherchez un trésor ");
+        System.out.println("Vous arrivez devant un rocher ayant une inscription mystérieuse...");
+        System.out.println("[Au fond des ruines Aztecs vous trouverez ce que vous cherchez. Gare à vous, l'aventure ne vas pas être si facile...]");
+        System.out.println("[Je vous conseille de.. ]");
+        System.out.println("Le reste du texte est illisible, donc vous continuez  plus profondément dans la jungle.");
+
+
+    }
+    public static void afficherPlan(){
 
     }
 }
